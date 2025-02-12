8a0642935c811578c7ee9713b069f180eed63753
teodor-varbanov <tvarbanov@outlook.com>
2025-02-12 18:22:54 +0200

    added the ability to specify the port from cmd

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP port")
+	flag.Parse()
+
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -14,7 +18,7 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Println("Starting server on", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
